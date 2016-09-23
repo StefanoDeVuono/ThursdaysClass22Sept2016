e0438f1e19887aeb1a62f12893fd06a62e4cc8d0
Stefano De Vuono <io@stefanodevuono.com>
2016-09-22 20:39:54 -0700

    objects on objects

--- class-examples.js
+++ class-examples.js
@@ -39,3 +39,28 @@ var Person = function(data){
 var newPerson = new Person({shirtColour: 'polka dot'});
 
 console.log(newPerson.shirtColour);
+
+// special stuff
+var advancedExample = {};
+Object.defineProperty(advancedExample, 'hiddenShirt', {
+  enumerable: false,
+   writable: false,
+   configurable: false,
+   value: 'black, but with stripes'
+});
+
+Object.defineProperty(advancedExample, 'shirtColour', {
+  enumerable: true,
+  writable: false,
+  configurable: true,
+  value: 'solid black'
+});
+
+advancedExample.shirtColour = 'blue'; // won't work, not writable
+
+Object.defineProperty(advancedExample, 'shirtColour', {value: 'mars black'});
+
+advancedExample.hiddenShirt; // gets us 'black, but with stripes'
+
+// WILL THROW ERROR!
+Object.defineProperty(advancedExample, 'hiddenShirt', {value: 'blue black'});
